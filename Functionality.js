db3e43d2d22874d441d95c567c12391181ae9f42
Francis Victor <francis@revolite.io>
2024-11-21 16:59:57 +0530

    Commit

--- Functionality.js
+++ Functionality.js
@@ -1,3 +1,19 @@
 function OpenMenu(){
     
 }
+
+
+window.onscroll = function(e) {scrollFunction(e)};
+
+function scrollFunction(e) {    
+    let element = document.getElementById("navbar");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        element.style.position = "fixed";
+        element.style.background="#474d52";
+          element.style.top = "0";
+          element.style.zIndex = 1;
+
+    }else{
+        element.style.background="none";
+    }
+}
